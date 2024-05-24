b6414210766c2faf025f49eaf4f37804764dad11
Oisee Hastin <hastin@oisee.cool>
2024-05-24 23:36:39 +0800

    massStyleUpadate

--- js/main.js
+++ js/main.js
@@ -17,6 +17,17 @@ async function addSpace() {
           for (let i = 0; i < shapes.items.length; i++) {
                let tmpObj = new Object();
                tmpObj.slideID = curSlideID;
+               console.log("start");
+               if (shapes.items[i].type != "GeometricShape") {
+                    console.log("not geoShape");
+                    continue;
+               }
+               shapes.items[i].textFrame.load("hasText");
+               await context.sync();
+               if (!shapes.items[i].textFrame.hasText) {
+                    console.log("not Text");
+                    continue;
+               }
                tmpObj.shapeID = shapes.items[i].id;
                let curRange = shapes.items[i].textFrame.textRange;
                shapes.items[i].textFrame.textRange.load("text");
@@ -354,6 +365,347 @@ async function applyItemStyle(presetIndex, posOpt, bySizeOpt) {
           });
           // Get the position information
           // Now you can use 'left' and 'top' to determine the position of the shape.
+     });
+}
+
+let titelStyle = {};
+async function readTitleStyle() {
+     await PowerPoint.run(async (context) => {
+          let selectedShapes = context.presentation.getSelectedShapes();
+          selectedShapes.load("items");
+          await context.sync();
+          selectedShapes.items[0].name = "Title textbox";
+          let textRange = selectedShapes.items[0].textFrame.textRange;
+          selectedShapes.items[0].load("textFrame");
+          await context.sync();
+          textRange.load("font");
+          await context.sync();
+          textRange.load("paragraphFormat");
+          await context.sync();
+          console.log(selectedShapes.items[0].textFrame.textRange.font);
+          // console.log(selectedShapes.items[0].type);
+          titelStyle = {
+               left: Math.round(selectedShapes.items[0].left),
+               top: Math.round(selectedShapes.items[0].top),
+               width: Math.round(selectedShapes.items[0].width),
+               height: Math.round(selectedShapes.items[0].height),
+               right: Math.round(selectedShapes.items[0].left + selectedShapes.items[0].width),
+               bottom: Math.round(selectedShapes.items[0].top + selectedShapes.items[0].height),
+               fontSize: selectedShapes.items[0].textFrame.textRange.font.size,
+               fontColor: selectedShapes.items[0].textFrame.textRange.font.color,
+               fontFamily: selectedShapes.items[0].textFrame.textRange.font.name,
+               bold: selectedShapes.items[0].textFrame.textRange.font.bold,
+               italic: selectedShapes.items[0].textFrame.textRange.font.italic,
+               xAlign: selectedShapes.items[0].textFrame.textRange.paragraphFormat.horizontalAlignment,
+               vAlign: selectedShapes.items[0].textFrame.verticalAlignment,
+               ml: selectedShapes.items[0].textFrame.leftMargin,
+               mr: selectedShapes.items[0].textFrame.rightMargin,
+               mt: selectedShapes.items[0].textFrame.topMargin,
+               mb: selectedShapes.items[0].textFrame.bottomMargin,
+          };
+     });
+}
+
+async function applyTitleStyle() {
+     let selectorEle = document.querySelectorAll('input[name="fontDropperTitle"]');
+     let applyFormat = [];
+
+     selectorEle.forEach((e) => {
+          applyFormat.push(e.checked);
+     });
+     console.log(applyFormat);
+     console.log(titelStyle);
+     await PowerPoint.run(async (context) => {
+          let selectedSlides = context.presentation.getSelectedSlides();
+          selectedSlides.load("items");
+          await context.sync();
+
+          for (let i = 0; i < selectedSlides.items.length; i++) {
+               slide = selectedSlides.items[i];
+               slide.load("shapes");
+               await context.sync();
+               let slideShapes = slide.shapes;
+               slideShapes.load("items");
+               await context.sync();
+               for (let k = 0; k < slideShapes.items.length; k++) {
+                    let shape = slideShapes.items[k];
+                    if (shape.type != "GeometricShape") {
+                         console.log("not geoShape");
+                         continue;
+                    }
+                    if (shape.top > ($("#titleJudge_Top").val() * 540) / 100) {
+                         continue;
+                    }
+                    if (shape.left > ($("#titleJudge_Left").val() * 960) / 100) {
+                         continue;
+                    }
+
+                    let curRange = shape.textFrame.textRange;
+                    try {
+                         shape.textFrame.textRange.load("font");
+                         await context.sync();
+                    } catch (err) {
+                         console.log("Err");
+                         continue;
+                    }
+
+                    shape.load("width,height");
+                    shape.name = "Title textbox";
+                    console.log("A");
+                    shape.width = titelStyle.width;
+                    shape.height = titelStyle.height;
+                    shape.left = titelStyle.left;
+                    shape.top = titelStyle.top;
+                    console.log("B");
+
+                    shape.textFrame.textRange.paragraphFormat.horizontalAlignment = titelStyle.xAlign;
+                    // shape.textFrame.verticalAlignment = PowerPoint.TextVerticalAlignment.top;
+                    console.log("H");
+                    if (applyFormat[0]) {
+                         shape.textFrame.textRange.font.name = titelStyle.fontFamily;
+                    }
+                    console.log("K");
+                    if (applyFormat[1]) {
+                         shape.textFrame.textRange.font.color = titelStyle.fontColor;
+                    }
+                    console.log("R");
+                    if (applyFormat[2]) {
+                         shape.textFrame.textRange.font.size = titelStyle.fontSize;
+                    }
+                    console.log("Z");
+                    if (applyFormat[3]) {
+                         shape.textFrame.textRange.font.bold = titelStyle.bold;
+                    }
+                    if (applyFormat[4]) {
+                         shape.textFrame.textRange.font.italic = titelStyle.italic;
+                    }
+                    if (applyFormat[5]) {
+                         shape.textFrame.verticalAlignment = titelStyle.vAlign;
+                    }
+                    shape.textFrame.leftMargin = titelStyle.ml;
+                    shape.textFrame.rightMargin = titelStyle.mr;
+                    shape.textFrame.topMargin = titelStyle.mt;
+                    shape.textFrame.bottomMargin = titelStyle.mb;
+                    console.log("End");
+               }
+          }
+     });
+}
+
+let refStyle = {};
+async function readRefStyle() {
+     await PowerPoint.run(async (context) => {
+          let selectedShapes = context.presentation.getSelectedShapes();
+          selectedShapes.load("items");
+          await context.sync();
+          let textRange = selectedShapes.items[0].textFrame.textRange;
+          selectedShapes.items[0].name = "Ref textbox";
+          selectedShapes.items[0].load("textFrame");
+          await context.sync();
+          textRange.load("font");
+          await context.sync();
+          textRange.load("paragraphFormat");
+          await context.sync();
+          console.log(selectedShapes.items[0].textFrame.textRange.font);
+          refStyle = {
+               left: Math.round(selectedShapes.items[0].left),
+               top: Math.round(selectedShapes.items[0].top),
+               width: Math.round(selectedShapes.items[0].width),
+               height: Math.round(selectedShapes.items[0].height),
+               right: Math.round(selectedShapes.items[0].left + selectedShapes.items[0].width),
+               bottom: Math.round(selectedShapes.items[0].top + selectedShapes.items[0].height),
+               fontSize: selectedShapes.items[0].textFrame.textRange.font.size,
+               fontColor: selectedShapes.items[0].textFrame.textRange.font.color,
+               fontFamily: selectedShapes.items[0].textFrame.textRange.font.name,
+               bold: selectedShapes.items[0].textFrame.textRange.font.bold,
+               italic: selectedShapes.items[0].textFrame.textRange.font.italic,
+               xAlign: selectedShapes.items[0].textFrame.textRange.paragraphFormat.horizontalAlignment,
+               vAlign: selectedShapes.items[0].textFrame.verticalAlignment,
+               ml: selectedShapes.items[0].textFrame.leftMargin,
+               mr: selectedShapes.items[0].textFrame.rightMargin,
+               mt: selectedShapes.items[0].textFrame.topMargin,
+               mb: selectedShapes.items[0].textFrame.bottomMargin,
+          };
+     });
+}
+
+async function applyRefStyle() {
+     let selectorEle = document.querySelectorAll('input[name="fontDropperRef"]');
+     let applyFormat = [];
+
+     selectorEle.forEach((e) => {
+          applyFormat.push(e.checked);
+     });
+
+     await PowerPoint.run(async (context) => {
+          let selectedSlides = context.presentation.getSelectedSlides();
+          selectedSlides.load("items");
+          await context.sync();
+
+          await context.sync();
+          selectedSlides.items.sort(function (a, b) {
+               return b.bottom - a.bottom;
+          });
+          console.log(selectedSlides.items);
+          for (let i = 0; i < selectedSlides.items.length; i++) {
+               slide = selectedSlides.items[i];
+               slide.load("shapes");
+               await context.sync();
+               let slideShapes = slide.shapes;
+               slideShapes.load("items");
+               await context.sync();
+               let curRefShape = null;
+               let curRefShapeBottom = null;
+               let curSlideRefShapes = [];
+               refObjCount = 0;
+               for (let k = 0; k < slideShapes.items.length; k++) {
+                    let shape = slideShapes.items[k];
+                    try {
+                         console.log("start");
+                         if (shape.type != "GeometricShape") {
+                              console.log("not geoShape");
+                              continue;
+                         }
+                         shape.textFrame.load("hasText");
+                         await context.sync();
+                         if (!shape.textFrame.hasText) {
+                              console.log("not Text");
+                              continue;
+                         }
+                         // console.log("loading pos");
+                         shape.load("width");
+                         await context.sync();
+                         shape.load("height");
+                         await context.sync();
+
+                         // console.log("pos");
+                         if (shape.top + shape.height < ((100 - $("#refJudge_Top").val()) * 540) / 100) {
+                              continue;
+                         }
+                         if (shape.left > ($("#refJudge_Left").val() * 960) / 100) {
+                              continue;
+                         }
+
+                         // let curRange = shape.textFrame.textRange;
+                         try {
+                              shape.load("textFrame");
+                              await context.sync();
+                              shape.textFrame.textRange.load("font");
+                              await context.sync();
+                              shape.textFrame.textRange.load("paragraphFormat");
+                              await context.sync();
+                         } catch (err) {
+                              console.log("Err");
+                              continue;
+                         }
+                         // console.log("loading text");
+                         shape.textFrame.textRange.load("text");
+                         await context.sync();
+                         // console.log(shape.textFrame.textRange.text.toString()[0]);
+                         // if (curRefShape == null) {
+                         shape = shape;
+                         shape.name = "Ref textbox";
+                         if (refObjCount > 0) {
+                              shape.name = "SubRef" + refObjCount;
+                         }
+                         // shape.bottom = shape.top + shape.height;
+                         curSlideRefShapes.push(shape);
+                         shape.textFrame.autoSizeSetting = PowerPoint.ShapeAutoSize.AutoSizeNone;
+                         shape.width = refStyle.width;
+                         shape.height = refStyle.height;
+                         shape.left = refStyle.left;
+                         shape.top = refStyle.top - refObjCount * 20;
+                         refObjCount++;
+                         // console.log("size");
+                         // console.log(shape.textFrame.autoSizeSetting);
+                         // console.log("autosize");
+                         // console.log(refStyle.ml);
+
+                         // console.log("margin");
+                         if (applyFormat[0]) {
+                              shape.textFrame.textRange.font.name = refStyle.fontFamily;
+                         }
+                         if (applyFormat[1]) {
+                              shape.textFrame.textRange.font.color = refStyle.fontColor;
+                         }
+                         if (applyFormat[2]) {
+                              shape.textFrame.textRange.font.size = refStyle.fontSize;
+                         }
+                         console.log(shape.textFrame.textRange.font.size);
+                         if (applyFormat[3]) {
+                              shape.textFrame.textRange.font.bold = refStyle.bold;
+                         }
+                         if (applyFormat[4]) {
+                              shape.textFrame.textRange.font.italic = refStyle.italic;
+                         }
+                         if (applyFormat[5]) {
+                              shape.textFrame.verticalAlignment = refStyle.vAlign;
+                         }
+                         // console.log("font");
+                         shape.textFrame.textRange.paragraphFormat.horizontalAlignment = refStyle.xAlign;
+                         // console.log("vA");
+                         shape.textFrame.leftMargin = refStyle.ml;
+                         shape.textFrame.rightMargin = refStyle.mr;
+                         shape.textFrame.topMargin = refStyle.mt;
+                         shape.textFrame.bottomMargin = refStyle.mb;
+                         // console.log("xA");
+                         // } else {
+                         //      // shape.textFrame.textRange.load("text");
+                         //      // await context.sync();
+                         //      // console.log(shape.textFrame.textRange.text);
+                         //      curRefShape.textFrame.textRange.load("text");
+                         //      await context.sync();
+                         //      console.log(curRefShape.textFrame.textRange.text);
+                         //      if (curRefShapeBottom < shape.top + shape.height) {
+                         //           let insertPoint = curRefShape.textFrame.textRange.text.length - 1;
+                         //           let tmpRange = curRefShape.textFrame.textRange.getSubstring(curRefShape.textFrame.textRange.text.length - 1, 1);
+                         //           tmpRange.load("text");
+                         //           await context.sync();
+                         //           console.log("A");
+                         //           console.log(tmpRange.text[0]);
+                         //           tmpRange.text = tmpRange.text[0] + "\n ";
+                         //           let insertRange = curRefShape.textFrame.textRange.getSubstring(insertPoint + 2, 1);
+                         //           insertRange.load("text");
+                         //           await context.sync();
+                         //           insertRange.text = shape.textFrame.textRange.text;
+                         //           // insertRange = shape.textFrame.textRange;
+                         //           // curRefShape.textFrame.textRange = +"\n" + curRefShape.shape.textFrame.textRangetextFrame.textRange;
+                         //           // console.log(curRefShape.textFrame.textRange.text.toString()[0]);
+                         //           // curRefShape.textFrame.textRange.text = curRefShape.textFrame.textRange.text + "\n" + shape.textFrame.textRange.text;
+                         //      } else {
+                         //           console.log("B: ");
+                         //           let insertPoint = 0;
+                         //           let tmpRange = curRefShape.textFrame.textRange.getSubstring(0, 1);
+                         //           tmpRange.load("text");
+                         //           await context.sync();
+                         //           tmpRange.text = " \n" + curRefShape.textFrame.textRange.text[0];
+                         //           let insertRange = curRefShape.textFrame.textRange.getSubstring(insertPoint, 1);
+                         //           insertRange.load("text");
+                         //           await context.sync();
+                         //           insertRange.text = shape.textFrame.textRange.text;
+                         //           // curRefShape.textFrame.textRange.text = shape.textFrame.textRange.text + "\n" + curRefShape.textFrame.textRange.text;
+                         //      }
+                         //      shape.delete();
+                         //      k--;
+                         //      console.log("k-");
+                         //      continue;
+                         // }
+                    } catch (err) {
+                         console.log(err);
+                    }
+                    console.log("nRound");
+               }
+               curSlideRefShapes.sort(function (a, b) {
+                    return b.bottom - a.bottom;
+               });
+               console.log(curSlideRefShapes);
+               for (let k = 0; k < curSlideRefShapes.length - 1; k++) {
+                    console.log(curSlideRefShapes[k].name);
+                    curSlideRefShapes[k].name = curSlideRefShapes[k].name + "-" + k;
+                    console.log(curSlideRefShapes[k].name);
+                    curSlideRefShapes[k].top = curSlideRefShapes[k].top - 5 * k;
+               }
+          }
      });
 }
 
